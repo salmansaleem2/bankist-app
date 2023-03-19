9f2a85d1a62f12bdd29e28406d2d3d85ea7443e8
salmansaleem2 <salmansaleem8523@gmail.com>
2023-03-19 17:06:44 +0500

    practise Event Propagation: Bubbling and Capturing

--- script.js
+++ script.js
@@ -168,3 +168,30 @@ setTimeout(() => {
 // h1.onmouseenter = function (e) {
 //   alert('addEventListener: Greqat! you are reading the heading :D');
 // };
+
+// capturing Phase and bubbling Phase
+
+// rgb(255,255,255)
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('Link', e.target, e.currentTarget);
+  console.log('Link', this === e.currentTarget);
+
+  // Stop Propagation
+  e.stopPropagation();
+});
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('Link', e.target, e.currentTarget);
+  console.log('Link', this === e.currentTarget);
+});
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('Link', e.target, e.currentTarget);
+  console.log('Link', this === e.currentTarget);
+});
